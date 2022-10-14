5a79832413f37d836454f4129b72b094c3b5ae7e
Rica Craver <rica.craver@yahoo.com>
2022-10-13 20:17:41 -0400

    Finished

---
+++ refresher.js
@@ -0,0 +1,17 @@
+var firstName = "Rica";
+var lastName = "Craver";
+
+let fullName = firstName + " " + lastName;
+console.log(firstName.concat(" ", lastName));
+
+var population = "672,351";
+console.log(population);
+
+let sum = population * 3;
+console.log(sum);
+
+const javaScriptIsCool = true;
+console.log(javaScriptIsCool);
+
+const rainbow = ['Red', 'orange', 'yellow', 'green'];
+console.log(rainbow);
